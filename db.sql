93b1544a209d51ef743104553ced13ee3f6fb5c7
Morten <morten.riedel@googlemail.com>
2014-05-22 22:36:45 +0200

    Tabelle User: - Spalte password auf varChar(255) gesetzt.
    vorheriger Wert war 20, ist aber zu klein für Hashing verfahren

--- db.sql
+++ db.sql
@@ -45,7 +45,7 @@ CREATE TABLE "user"
 (
   id serial NOT NULL,
   username character varying(20) NOT NULL,
-  password character varying(20) NOT NULL,
+  password character varying(255) NOT NULL,
   saltvalue character varying(5) NOT NULL,
   CONSTRAINT user_pkey PRIMARY KEY (id)
 )
